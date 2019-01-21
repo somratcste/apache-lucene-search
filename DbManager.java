5d153432bc9ab415c066b7b0105f9b88e8dba92c
somratcste <somratcste@gmail.com>
2019-01-21 15:09:49 +0600

    change vm password for database

--- DbManager.java
+++ DbManager.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class DbManager {
         static private String databaseURL = "jdbc:mysql://localhost:3306/";
         static private String dbname = "ad";
-        static private String username = "root";
-        static private String password = "";
+        static private String username = "ad";
+        static private String password = "ad";
 	
 	/**
 	 * Opens a database connection
